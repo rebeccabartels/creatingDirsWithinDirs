f8a6ea42749faa558d23b0cb1c4c79f98be18bbf
riley <rebeccakbartels@gmail.com>
2019-10-26 11:12:40 -0400

    fixed the function so that you don't have to hard code it.

--- solution.py
+++ solution.py
@@ -12,16 +12,12 @@ def main():
     # create CyberSecurity-Notes directory
     # os.mkdir('CyberSecurity-Notes')
     root_path = '/Users/rebeccabartels/cyber-homework/Unit 4 Advanced Python/HW04-Python'
-    folders = ['Week1', 'Week2', 'Week3', 'Week4', 'Week5', 'Week6', 'Week7', 'Week8', 'Week9', 'Week10', 'Week11', 'Week12',
-               'Week13', 'Week14', 'Week15', 'Week16', 'Week17', 'Week18', 'Week19', 'Week20', 'Week21', 'Week22', 'Week23', 'Week24']
+    # folders = ['Week1', 'Week2', 'Week3', 'Week4', 'Week5', 'Week6', 'Week7', 'Week8', 'Week9', 'Week10', 'Week11', 'Week12',
+    # 'Week13', 'Week14', 'Week15', 'Week16', 'Week17', 'Week18', 'Week19', 'Week20', 'Week21', 'Week22', 'Week23', 'Week24']
     for root, dirs, files in os.walk('/Users/rebeccabartels/cyber-homework/Unit 4 Advanced Python/HW04-Python'):
         print("hello")
-        for f in folders:
-            if count < 23:
-                os.mkdir('Day1')
-                os.mkdir('Day2')
-                os.mkdir('Day3')
-                count += 1
+        for i in range(24):
+            os.makedirs("Week %d\r\n" % (i+1))
 
 
 main()
