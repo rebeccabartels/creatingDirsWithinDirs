527152e445c349b3ebe861c8165070cf43cbefff
riley <rebeccakbartels@gmail.com>
2019-10-26 14:51:11 -0400

    solution to pt 1

--- mysolution.py
+++ mysolution.py
@@ -8,26 +8,29 @@ import os
 
 
 def main():
-    count = range(1, 25)
     # create CyberSecurity-Notes directory
     # os.mkdir('CyberSecurity-Notes')
     root_path = '/Users/rebeccabartels/cyber-homework/Unit 4 Advanced Python/HW04-Python'
     for root, dirs, files in os.walk('/Users/rebeccabartels/cyber-homework/Unit 4 Advanced Python/HW04-Python'):
-        x = len(count)
-        print("You have " + str(x) + " directories here.")
-        break
-        if x == 24:
-            isTrue = True
-            print("Your files have been created.")
-            # if isTrue == True:
-            # for root, dirs, files in os.walk(root_path):
-            # print(dirs)
-            # print(files)
+        #    print("****************")
+        #    print("You have " + str(x) + " directories here.")
+        #    break
+        # if x == 24:
+        #    isTrue = True
+        #    print("Your files have been created.")
+        #    print("****************")
+        #    if len(os.listdir(root_path)) == 0:
+        #        print("Directory is empty")
+        #    else:
+        #        print("Directory is complete")
 
-        else:
-            print("Let's get started...")
-            for i in range(24):
-                os.makedirs("Week %d\r\n" % (i+1))
+        # else:
+        #    print("Let's get started...")
+        for i in range(1, 25):
+            #os.makedirs("Week %d\r\n" % (i+1))
+            for dir in dirs:
+                for j in range(3):
+                    os.makedirs("Week " + str(i) + "/Day- %d\r\n" % (j+1))
 
 
 main()
