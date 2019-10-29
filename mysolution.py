cb528d1bf4a313c03df83bad8d3128b8fe9f42f6
riley <rebeccakbartels@gmail.com>
2019-10-29 16:18:58 -0400

    final solution

--- mysolution.py
+++ mysolution.py
@@ -7,30 +7,25 @@ import os
 # print(os.listdir())
 
 
-def main():
-    # create CyberSecurity-Notes directory
-    # os.mkdir('CyberSecurity-Notes')
-    root_path = '/Users/rebeccabartels/cyber-homework/Unit 4 Advanced Python/HW04-Python'
-    for root, dirs, files in os.walk('/Users/rebeccabartels/cyber-homework/Unit 4 Advanced Python/HW04-Python'):
-        #    print("****************")
-        #    print("You have " + str(x) + " directories here.")
-        #    break
-        # if x == 24:
-        #    isTrue = True
-        #    print("Your files have been created.")
-        #    print("****************")
-        #    if len(os.listdir(root_path)) == 0:
-        #        print("Directory is empty")
-        #    else:
-        #        print("Directory is complete")
+import os
+# create the CyberSecurity Notes folder and also set it to a variable called notes_folder
 
-        # else:
-        #    print("Let's get started...")
-        for i in range(1, 25):
-            #os.makedirs("Week %d\r\n" % (i+1))
-            for dir in dirs:
-                for j in range(3):
-                    os.makedirs("Week " + str(i) + "/Day- %d\r\n" % (j+1))
+
+def main():
+    notes_folder = os.mkdir('CyberSecurity-Notes')
+    # create a for loop to create the weekly folders in a range
+    # remember that a range will start at the first argument in the function but will stop right before the second argument, so you must set the range from 1 to 25 in order for it to make folders with the numbers 1 through 24
+    for x in range(1, 25):
+        # use the mkdir() function to create a folder for each week
+        # mkdir() will only work if the notes_folder 'CyberSecurity-Notes' already exists
+        # use concatenation (+) to add the variable x which will increase by 1 each time in the for loop
+        # use the str() function to turn the integer into a string
+        os.mkdir('CyberSecurity-Notes/Week-' + str(x))
+        # remember that a range will start at the first argument in the function but will stop right before the second argument, so you must set the range from 1 to 4 in order to have a folder create for days 1 through 4
+        for y in range(1, 4):
+            # use concatenation (+) to add the variable y which will increase by 1 each time in the for loop
+            # use the str() function to turn the integer into a string
+            os.mkdir('CyberSecurity-Notes/Week-' + str(x) + '/Day-' + str(y))
 
 
 main()
